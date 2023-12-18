474d3b26621f41f0e09ba7e480b688e90b6e41f2
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-18 18:22:24 +0100

    ADD function store for save in DB new comics

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -27,6 +27,17 @@ class ComicController extends Controller
     {
         $data = $request->all();
 
-        dd($data);
+        $new_comic = new Comic();
+        $new_comic->title = $data['title'];
+        $new_comic->description = $data['description'];
+        $new_comic->thumb = $data['thumb'];
+        $new_comic->price = $data['price'];
+        $new_comic->series = $data['series'];
+        $new_comic->sale_date = $data['sale_date'];
+        $new_comic->type = $data['type'];
+        $new_comic->save();
+
+        // dd($new_comic);
+        return redirect()->route('comics.show', $new_comic);
     }
 }
