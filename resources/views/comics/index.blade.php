e7b2d22dfa411abb8e43a75ede4599b428b2e48b
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-18 17:24:59 +0100

    Print to page all comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,35 @@
 
 @section('content')
     <section>
-        <h1>Comics page content</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Comics page content</h1>
+                </div>
+            </div>
+            <div class="row row-gap-5">
+                @forelse ($comics as $comic)
+                    <div class="col-6">
+                        <div class="card">
+                            <img src="{{ $comic->thumb}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $comic->title }}</h5>
+                                <p class="card-text">{{ $comic->description}}</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Price: {{ $comic->price}}</li>
+                                <li class="list-group-item">Series: {{ $comic->series}}</li>
+                                <li class="list-group-item">Sale date: {{ $comic->sale_date}}</li>
+                                <li class="list-group-item">Type: {{ $comic->type}}</li>
+                               
+                            </ul>
+                        </div>
+                    </div>
+                @empty
+                    <h2>No comics</h2>
+                @endforelse
+                
+            </div>
+        </div>
     </section>
 @endsection
